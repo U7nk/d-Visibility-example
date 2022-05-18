022d55299557a9fd92e6a270659fd2f45b1cccaa
Kliment Nechaev <69924108+U7nk@users.noreply.github.com>
2022-05-18 17:56:24 +0500

    Add files via upload

---
+++ NET Framework 4.6.1 Example/WpfApp3/MainVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp3
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Visibility Visibility { get; set; }
+        public string Stringg { get; set; } = "Boo";
+        public MainVM()
+        {
+            Visibility = Visibility.Collapsed;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Visibility"));
+        }
+    }
+}
